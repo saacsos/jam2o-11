f2582fec4d7b1e29f2b219c835ad8cbd20bbade4
Saac Sornchai <sornchai.la@ku.th>
2023-09-12 09:30:43 +0700

    tailwind config and some api

--- pages/artists/index.vue
+++ pages/artists/index.vue
@@ -1,3 +1,19 @@
 <template>
   <h2>Artists</h2>
+
+  <div v-if="pending">
+    Pending...
+  </div>
+  <div v-else v-for="artist in artists" :key="artist.id">
+    <div class="m-4 text-blue-200">
+      <nuxt-link :to="`/artists/${artist.id}`">
+        {{ artist.name }}
+      </nuxt-link>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+const { data: artists, pending } = await useLazyFetch<any>("http://localhost/api/artist")
+
+</script>
