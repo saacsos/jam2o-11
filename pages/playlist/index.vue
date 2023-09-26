af0404e069e758e8b05d85ea91463c29b7b5b4b7
Saac Sornchai <sornchai.la@ku.th>
2023-09-26 14:06:07 +0700

    frontend authorization

---
+++ pages/playlist/index.vue
@@ -0,0 +1,15 @@
+<template>
+  {{ auth.user.name }}
+  <div>
+    <div v-for="playlist in playlists" :key="playlist.id">
+      {{ playlist.name }} [{{ playlist.accessibility }}]
+    </div>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import { useAuthStore } from '~/stores/useAuthStore'
+const auth = useAuthStore()
+const { data: playlists, error } = await useMyFetch<any>('playlist', {})
+</script>
